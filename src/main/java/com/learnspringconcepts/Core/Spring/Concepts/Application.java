77b1fedb014ad67e9e958ae0be878821ee207aaa
ashutosh7131 <ashutosh713@outlook.com>
2023-11-12 02:17:10 +0800

    fifth commit add new game

--- src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application.java
+++ src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application.java
@@ -2,6 +2,7 @@ package com.learnspringconcepts.Core.Spring.Concepts;
 
 import com.learnspringconcepts.Core.Spring.Concepts.game.GameRunner;
 import com.learnspringconcepts.Core.Spring.Concepts.game.MarioGame;
+import com.learnspringconcepts.Core.Spring.Concepts.game.PacManGame;
 import com.learnspringconcepts.Core.Spring.Concepts.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +14,9 @@ public class Application {
 
 
 		//SpringApplication.run(Application.class, args);
-		var game = new MarioGame();
+		//var game = new MarioGame();
 		//var game = new SuperContraGame();
+		var game = new PacManGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
