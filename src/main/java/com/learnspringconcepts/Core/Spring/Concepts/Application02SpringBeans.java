a9ae4af6418b23fe5795bc0823d9913c55b02b10
ashutosh7131 <ashutosh713@outlook.com>
2023-11-14 00:44:45 +0800

    16th commit for new project

--- src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application02SpringBeans.java
+++ src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application02SpringBeans.java
@@ -1,10 +1,26 @@
 package com.learnspringconcepts.Core.Spring.Concepts;
 
-import com.learnspringconcepts.Core.Spring.Concepts.config.GamingConfig;
 import com.learnspringconcepts.Core.Spring.Concepts.game.GameRunner;
 import com.learnspringconcepts.Core.Spring.Concepts.game.GamingConsole;
+import com.learnspringconcepts.Core.Spring.Concepts.game.PacManGame;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class GamingConfig {
+
+	@Bean
+	public GamingConsole game(){
+		return new PacManGame();
+	}
+
+	@Bean
+	public GameRunner gameRunner(GamingConsole game){
+		return new GameRunner(game);
+	}
+}
 
 @SpringBootApplication
 public class Application02SpringBeans {
