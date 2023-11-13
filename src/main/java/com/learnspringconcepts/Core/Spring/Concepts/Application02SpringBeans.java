9c17a1c939a099551989b18da93ab6a7dd6ee5ba
ashutosh7131 <ashutosh713@outlook.com>
2023-11-14 01:12:10 +0800

    18th commit for showing @Component annotation

--- src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application02SpringBeans.java
+++ src/main/java/com/learnspringconcepts/Core/Spring/Concepts/Application02SpringBeans.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
 @Configuration
 public class Application02SpringBeans {
 
