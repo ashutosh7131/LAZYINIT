6ded8a464b7367a5a357db3df2adbca35217bd2c
ashutosh7131 <ashutosh713@outlook.com>
2023-11-13 21:34:48 +0800

    13th commit try with resources

--- src/main/java/com/learnspringconcepts/Core/Spring/Concepts/helloWorld/HelloWorldConfiguration.java
+++ src/main/java/com/learnspringconcepts/Core/Spring/Concepts/helloWorld/HelloWorldConfiguration.java
@@ -1,4 +1,4 @@
-package com.learnspringconcepts.Core.Spring.Concepts;
+package com.learnspringconcepts.Core.Spring.Concepts.helloWorld;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
